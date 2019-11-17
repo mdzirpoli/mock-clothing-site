ac3ab47cb710c22e1a2026033799ba1c435ad8a8
mdzirpoli <mdzirpoli@gmail.com>
2019-11-17 15:20:12 -0500

    Sign In page
    Sign In page methods and tests

---
+++ Tests/Sign_In_Page_Tests.py
@@ -0,0 +1,69 @@
+"""
+Created on November 16, 2019
+
+@author: Mark Zirpoli
+
+This module contains the unit tests for the Sign In page
+"""
+
+from Pages.Sign_In_Page import SignInPage
+from selenium import webdriver
+import unittest
+import time
+
+url = "http://automationpractice.com/index.php"
+
+
+class SignInPageTests(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(5)
+        self.driver.get(url)
+        driver = self.driver
+        sign_in = SignInPage(driver)
+        sign_in.click_sign_in_menu_button()
+
+    @unittest.skip("pass")
+    def test_input_create_account_email_address_textbox(self):
+        driver = self.driver
+        sign_in = SignInPage(driver)
+        sign_in.input_create_account_email_address_textbox("joesmith@email.com")
+
+    @unittest.skip("pass")
+    def test_click_create_an_account_button(self):
+        driver = self.driver
+        sign_in = SignInPage(driver)
+        sign_in.click_create_an_account_button()
+
+    @unittest.skip("pass")
+    def test_input_email_address_textbox(self):
+        driver = self.driver
+        sign_in = SignInPage(driver)
+        sign_in.input_email_address_textbox("email@email.com")
+
+    @unittest.skip("pass")
+    def test_input_password_textbox(self):
+        driver = self.driver
+        sign_in = SignInPage(driver)
+        sign_in.input_password_textbox("abcd1234")
+
+    @unittest.skip("pass")
+    def test_click_sign_in_button(self):
+        driver = self.driver
+        sign_in = SignInPage(driver)
+        sign_in.click_sign_in_button()
+
+    @unittest.skip("pass")
+    def test_forgot_your_password_link(self):
+        driver = self.driver
+        sign_in = SignInPage(driver)
+        sign_in.click_forgot_your_password_link()
+
+    def tearDown(self):
+        time.sleep(2)
+        self.driver.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
