6cb144d8d01deae5ceb1930bcdec291683535915
mdzirpoli <mdzirpoli@gmail.com>
2020-01-23 20:35:02 -0500

    Refactor Sign In page methods and tests
    Refactor Sign In page methods and tests

--- Pages/SignInPage.py
+++ Pages/SignInPage.py
@@ -5,11 +5,10 @@ Created on November 16, 2019
 
 This module contains methods for the Sign In page
 """
-from Pages.BasePageObject import BasePage
 from selenium.webdriver.common.by import By
 
 
-class SignInPage(BasePage):
+class SignInPage(object):
     """
     Page object for Sign In page
     """
@@ -24,26 +23,25 @@ class SignInPage(BasePage):
     FORGOT_YOUR_PASSWORD_LINK = (By.XPATH, "//*[@id='login_form']/div/p[1]/a")
 
     def __init__(self, driver):
-        super().__init__(driver)
         self.driver = driver
 
     def click_sign_in_menu_button(self):
-        self.driver.find_element(*SignInPage.SIGN_IN_MENU_BUTTON).click()
+        self.driver.find_element(*self.SIGN_IN_MENU_BUTTON).click()
 
     def input_create_account_email_address_textbox(self, email):
-        self.driver.find_element(*SignInPage.CREATE_ACCOUNT_EMAIL_ADDRESS_TEXTBOX).send_keys(email)
+        self.driver.find_element(*self.CREATE_ACCOUNT_EMAIL_ADDRESS_TEXTBOX).send_keys(email)
 
     def click_create_an_account_button(self):
-        self.driver.find_element(*SignInPage.CREATE_AN_ACCOUNT_BUTTON).click()
+        self.driver.find_element(*self.CREATE_AN_ACCOUNT_BUTTON).click()
 
     def input_email_address_textbox(self, email):
-        self.driver.find_element(*SignInPage.EMAIL_ADDRESS_TEXTBOX).send_keys(email)
+        self.driver.find_element(*self.EMAIL_ADDRESS_TEXTBOX).send_keys(email)
 
     def input_password_textbox(self, password):
-        self.driver.find_element(*SignInPage.PASSWORD_TEXTBOX).send_keys(password)
+        self.driver.find_element(*self.PASSWORD_TEXTBOX).send_keys(password)
 
     def click_sign_in_button(self):
-        self.driver.find_element(*SignInPage.SIGN_IN_BUTTON).click()
+        self.driver.find_element(*self.SIGN_IN_BUTTON).click()
 
     def click_forgot_your_password_link(self):
-        self.driver.find_element(*SignInPage.FORGOT_YOUR_PASSWORD_LINK).click()
+        self.driver.find_element(*self.FORGOT_YOUR_PASSWORD_LINK).click()
--- Tests/SignInPageTests.py
+++ Tests/SignInPageTests.py
@@ -19,9 +19,8 @@ class SignInPageTests(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.get(url)
-        driver = self.driver
-        sign_in = SignInPage(driver)
-        sign_in.click_sign_in_menu_button()
+        self.sign_in = SignInPage(self.driver)
+        self.sign_in.click_sign_in_menu_button()
 
     def tearDown(self):
         time.sleep(2)
@@ -29,39 +28,27 @@ class SignInPageTests(unittest.TestCase):
 
     @unittest.skip("pass")
     def test_input_create_account_email_address_textbox(self):
-        driver = self.driver
-        sign_in = SignInPage(driver)
-        sign_in.input_create_account_email_address_textbox("joesmith@email.com")
+        self.sign_in.input_create_account_email_address_textbox("joesmith@email.com")
 
     @unittest.skip("pass")
     def test_click_create_an_account_button(self):
-        driver = self.driver
-        sign_in = SignInPage(driver)
-        sign_in.click_create_an_account_button()
+        self.sign_in.click_create_an_account_button()
 
     @unittest.skip("pass")
     def test_input_email_address_textbox(self):
-        driver = self.driver
-        sign_in = SignInPage(driver)
-        sign_in.input_email_address_textbox("email@email.com")
+        self.sign_in.input_email_address_textbox("email@email.com")
 
     @unittest.skip("pass")
     def test_input_password_textbox(self):
-        driver = self.driver
-        sign_in = SignInPage(driver)
-        sign_in.input_password_textbox("abcd1234")
+        self.sign_in.input_password_textbox("abcd1234")
 
     @unittest.skip("pass")
     def test_click_sign_in_button(self):
-        driver = self.driver
-        sign_in = SignInPage(driver)
-        sign_in.click_sign_in_button()
+        self.sign_in.click_sign_in_button()
 
     @unittest.skip("pass")
     def test_forgot_your_password_link(self):
-        driver = self.driver
-        sign_in = SignInPage(driver)
-        sign_in.click_forgot_your_password_link()
+        self.sign_in.click_forgot_your_password_link()
 
 
 if __name__ == '__main__':
