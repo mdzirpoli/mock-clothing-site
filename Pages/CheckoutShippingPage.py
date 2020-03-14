f741122b2a6f8b54ab96b9dd1481b01ece0179ba
mdzirpoli <mdzirpoli@gmail.com>
2020-03-14 13:00:43 -0400

    Checkout Shipping page methods and tests
    Checkout Shipping page methods and tests

---
+++ Pages/CheckoutShippingPage.py
@@ -0,0 +1,35 @@
+"""
+Created on March 14, 2020
+
+@author: Mark Zirpoli
+
+This module contains methods for the Checkout Shipping page
+"""
+from selenium.webdriver.common.by import By
+
+
+class CheckoutShippingPage(object):
+    """
+    Page object for Checkout Shipping page
+    """
+
+    # Checkout Shipping page locators
+    CHECKOUT_SHIPPING_TERMS_OF_SERVICE_CHECKBOX = (By.ID, "cgv")
+    CHECKOUT_SHIPPING_READ_THE_TERMS_OF_SERVICE_LINK = (By.XPATH, "//*[contains(text(), 'Read the Terms of Service')]")
+    CHECKOUT_SHIPPING_CONTINUE_SHOPPING_LINK = (By.XPATH, "//*[@title='Previous']")
+    CHECKOUT_SHIPPING_PROCEED_TO_CHECKOUT_BUTTON = (By.XPATH, "//*[@id='form']/p/button/span")
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click_checkout_shipping_terms_of_service_checkbox(self):
+        self.driver.find_element(*self.CHECKOUT_SHIPPING_TERMS_OF_SERVICE_CHECKBOX).click()
+
+    def click_checkout_shipping_read_the_terms_of_service(self):
+        self.driver.find_element(*self.CHECKOUT_SHIPPING_READ_THE_TERMS_OF_SERVICE_LINK).click()
+
+    def click_checkout_shipping_continue_shopping_link(self):
+        self.driver.find_element(*self.CHECKOUT_SHIPPING_CONTINUE_SHOPPING_LINK).click()
+
+    def click_checkout_shipping_proceed_to_checkout_button(self):
+        self.driver.find_element(*self.CHECKOUT_SHIPPING_PROCEED_TO_CHECKOUT_BUTTON).click()
---
+++ Tests/CheckoutShippingPageTests.py
@@ -0,0 +1,73 @@
+"""
+Created on March 14, 2020
+
+@author: Mark Zirpoli
+
+This module contains the unit tests for the Checkout Shipping page
+"""
+
+from Pages.ProductDetailsPage import ProductDetailsPage
+from Pages.Components.Modals.AddToCartModal import AddToCartModal
+from Pages.CheckoutSummaryPage import CheckoutSummaryPage
+from Pages.CheckoutAddressPage import CheckoutAddressPage
+from Pages.CheckoutShippingPage import CheckoutShippingPage
+from Pages.SignInPage import SignInPage
+from Pages.WomenPage import WomenPage
+from selenium import webdriver
+import unittest
+import time
+
+url = "http://automationpractice.com/index.php"
+
+
+class CheckoutShippingPageTests(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get(url)
+        self.sign_in = SignInPage(self.driver)
+        self.sign_in.click_sign_in_menu_button()
+        self.sign_in.input_email_address_textbox("test1940@gmail.com")
+        self.sign_in.input_password_textbox("abcd1234")
+        self.sign_in.click_sign_in_button()
+        self.women = WomenPage(self.driver)
+        self.women.click_women_nav_menu_button()
+        self.women.click_blouse_thumbnail()
+        self.product_details = ProductDetailsPage(self.driver)
+        self.product_details.click_product_details_page_add_to_cart_button()
+        time.sleep(2)
+        self.cart_modal = AddToCartModal(self.driver)
+        self.cart_modal.click_add_to_cart_modal_proceed_to_checkout_button()
+        self.summary = CheckoutSummaryPage(self.driver)
+        self.summary.click_checkout_summary_proceed_to_checkout_button()
+        self.address = CheckoutAddressPage(self.driver)
+        self.address.click_checkout_address_proceed_to_checkout_button()
+        self.shipping = CheckoutShippingPage(self.driver)
+
+    def tearDown(self):
+        time.sleep(2)
+        self.driver.close()
+
+    @unittest.skip("pass")
+    def test_click_checkout_shipping_terms_of_service_checkbox(self):
+        time.sleep(2)
+        self.shipping.click_checkout_shipping_terms_of_service_checkbox()
+
+    @unittest.skip("pass")
+    def test_click_checkout_shipping_read_the_terms_of_service(self):
+        time.sleep(2)
+        self.shipping.click_checkout_shipping_read_the_terms_of_service()
+
+    @unittest.skip("pass")
+    def test_click_checkout_shipping_continue_shopping_link(self):
+        time.sleep(2)
+        self.shipping.click_checkout_shipping_continue_shopping_link()
+
+    @unittest.skip("pass")
+    def test_click_checkout_shipping_proceed_to_checkout_button(self):
+        time.sleep(2)
+        self.shipping.click_checkout_shipping_proceed_to_checkout_button()
+
+
+if __name__ == '__main__':
+    unittest.main()
