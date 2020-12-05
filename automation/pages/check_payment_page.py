0d9d2e0c36dec54f84aa0dad814032863a5318f5
mdzirpoli <mdzirpoli@gmail.com>
2020-12-05 15:51:16 -0500

    Refactor check_payment_page with elements.py
    Refactor check_payment_page with elements.py

--- automation/pages/check_payment_page.py
+++ automation/pages/check_payment_page.py
@@ -1,33 +1,35 @@
 """
 Created on May 30, 2020
+Modified on December 5, 2020
 
 @author: Mark Zirpoli
 
 This module contains methods for the Check Payment page
 """
-from selenium.webdriver.common.by import By
+from automation.elements import Button, Link
 
 
 class CheckPaymentPage(object):
     """
     Page object for Check Payment page
     """
-
-    # Check Payment page locators
-    CHECK_PAYMENT_OTHER_PAYMENT_METHODS_LINK = (By.XPATH, "// *[ @ id = 'cart_navigation'] / a")
-    CHECK_PAYMENT_I_CONFIRM_MY_ORDER_BUTTON = (By.XPATH, "//*[contains(text(), 'I confirm my order')]")
-
     def __init__(self, driver):
         self.driver = driver
 
     def click_check_payment_other_payment_methods_link(self):
-        self.driver.find_element(*self.CHECK_PAYMENT_OTHER_PAYMENT_METHODS_LINK).click()
+        check_payment_other_payment_methods = \
+            Link(self.driver, xpath="// *[ @ id = 'cart_navigation'] / a")
+        check_payment_other_payment_methods.click()
 
     def click_check_payment_i_confirm_my_order_button(self):
-        self.driver.find_element(*self.CHECK_PAYMENT_I_CONFIRM_MY_ORDER_BUTTON).click()
+        check_payment_confirm_my_order = \
+            Button(self.driver,
+                   xpath="//*[contains(text(), 'I confirm my order')]")
+        check_payment_confirm_my_order.click()
 
     def verify_check_payment_text(self, text):
         try:
-            self.driver.find_element_by_xpath("//*[contains(text(),'" + text + "')]")
+            self.driver.find_element_by_xpath("//*[contains(text(),'"
+                                              + text + "')]")
         except Exception:
-            raise Exception(f"The text cannot be found")
+            raise Exception(f'{"The text cannot be found"}')
