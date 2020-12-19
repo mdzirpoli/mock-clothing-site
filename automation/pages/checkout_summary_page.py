be90f796d53dac4c7ac32406769b1ad4b89569b4
mdzirpoli <mdzirpoli@gmail.com>
2020-12-19 15:59:11 -0500

    Refactor checkout_summary_page with elements.py
    Refactor checkout_summary_page with elements.py

--- automation/pages/checkout_summary_page.py
+++ automation/pages/checkout_summary_page.py
@@ -1,45 +1,52 @@
 """
 Created on February 29, 2020
+Modified on December 19, 2020
 
 @author: Mark Zirpoli
 
 This module contains methods for the Checkout Summary page
 """
 
-from selenium.webdriver.common.by import By
+from automation.elements import Button, TextField, Link
 
 
 class CheckoutSummaryPage(object):
     """
     Page object for Checkout Summary page
     """
-
-    # Checkout Summary page locators
-    CHECKOUT_SUMMARY_QUANTITY_TEXTBOX = (By.XPATH, "//*[@id='product_2_7_0_272223']/td[5]/input[2]")
-    CHECKOUT_SUMMARY_QUANTITY_MINUS_BUTTON = (By.XPATH, "//*[@id='cart_quantity_down_2_7_0_272223']/span/i")
-    CHECKOUT_SUMMARY_QUANTITY_PLUS_BUTTON = (By.XPATH, "//*[@id='cart_quantity_up_2_7_0_272223']/span")
-    CHECKOUT_SUMMARY_TRASHCAN_BUTTON = (By.XPATH, "//*[@id='2_7_0_272223']/i")
-    CHECKOUT_SUMMARY_PROCEED_TO_CHECKOUT_BUTTON = (By.XPATH, "//*[@id='center_column']/p[2]/a[1]/span")
-    CHECKOUT_SUMMARY_CONTINUE_SHOPPING_LINK = (By.XPATH, "//*[@id='center_column']/p[2]/a[2]")
-
     def __init__(self, driver):
         self.driver = driver
 
     def input_checkout_summary_quantity_textbox(self, quantity):
-        self.driver.find_element(*self.CHECKOUT_SUMMARY_QUANTITY_TEXTBOX).clear()
-        self.driver.find_element(*self.CHECKOUT_SUMMARY_QUANTITY_TEXTBOX).send_keys(quantity)
+        checkout_summary_quantity = \
+            TextField(self.driver,
+                      xpath="//*[@id='product_2_7_0_272223']/td[5]/input[2]")
+        checkout_summary_quantity.input_text(quantity)
 
     def click_checkout_summary_minus_button(self):
-        self.driver.find_element(*self.CHECKOUT_SUMMARY_QUANTITY_MINUS_BUTTON).click()
+        checkout_summary_minus = \
+            Button(self.driver,
+                   xpath="//*[@id='cart_quantity_down_2_7_0_272223']/span/i")
+        checkout_summary_minus.click()
 
     def click_checkout_summary_plus_button(self):
-        self.driver.find_element(*self.CHECKOUT_SUMMARY_QUANTITY_PLUS_BUTTON).click()
+        checkout_summary_plus = \
+            Button(self.driver,
+                   xpath="//*[@id='cart_quantity_up_2_7_0_272223']/span")
+        checkout_summary_plus.click()
 
     def click_checkout_summary_trashcan_button(self):
-        self.driver.find_element(*self.CHECKOUT_SUMMARY_TRASHCAN_BUTTON).click()
+        checkout_summary_trashcan = \
+            Button(self.driver, xpath="//*[@id='2_7_0_272223']/i")
+        checkout_summary_trashcan.click()
 
     def click_checkout_summary_proceed_to_checkout_button(self):
-        self.driver.find_element(*self.CHECKOUT_SUMMARY_PROCEED_TO_CHECKOUT_BUTTON).click()
+        checkout_summary_proceed_to_checkout = \
+            Button(self.driver,
+                   xpath="//*[@id='center_column']/p[2]/a[1]/span")
+        checkout_summary_proceed_to_checkout.click()
 
     def click_checkout_summary_continue_shopping_link(self):
-        self.driver.find_element(*self.CHECKOUT_SUMMARY_CONTINUE_SHOPPING_LINK).click()
+        checkout_summary_continue_shopping = \
+            Link(self.driver, xpath="//*[@id='center_column']/p[2]/a[2]")
+        checkout_summary_continue_shopping.click()
