fea271ca9b185999ec745bfce0770cfa2fc7ffb9
mdzirpoli <mdzirpoli@gmail.com>
2020-12-19 16:37:31 -0500

    Refactor send_to_a_friend_modal with elements.py
    Refactor send_to_a_friend_modal with elements.py

--- automation/pages/components/modals/send_to_a_friend_modal.py
+++ automation/pages/components/modals/send_to_a_friend_modal.py
@@ -1,44 +1,46 @@
 """
 Created on February 15, 2020
+Modified on December 19, 2020
 
 @author: Mark Zirpoli
 
 This module contains methods for the Send To A Friend modal
 """
 
-from selenium.webdriver.common.by import By
+from automation.elements import Button, TextField, Link
 
 
 class SendToAFriendModal(object):
     """
     Page object for Send To A Friend Modal
     """
-
-    # Send To a Friend Modal locators
-    SEND_TO_A_FRIEND_LINK = (By.ID, "send_friend_button")
-    NAME_OF_YOUR_FRIEND_TEXTBOX = (By.ID, "friend_name")
-    EMAIL_ADDRESS_OF_YOUR_FRIEND_TEXTBOX = (By.ID, "friend_email")
-    SEND_TO_A_FRIEND_SEND_BUTTON = (By.ID, "sendEmail")
-    SEND_TO_A_FRIEND_CANCEL_BUTTON = (By.XPATH, "//*[@id='send_friend_form_content']/p/a")
-    SEND_TO_A_FRIEND_X_BUTTON = (By.XPATH, "//*[@title='Close']")
-
     def __init__(self, driver):
         self.driver = driver
 
     def click_send_to_a_friend_link(self):
-        self.driver.find_element(*self.SEND_TO_A_FRIEND_LINK).click()
+        send_to_a_friend = Link(self.driver, div_id="send_friend_button")
+        send_to_a_friend.click()
 
     def input_name_of_your_friend_textbox(self, friend):
-        self.driver.find_element(*self.NAME_OF_YOUR_FRIEND_TEXTBOX).send_keys(friend)
+        name_of_your_friend = TextField(self.driver, div_id="friend_name")
+        name_of_your_friend.input_text(friend)
 
     def input_email_address_of_your_friend_textbox(self, email):
-        self.driver.find_element(*self.EMAIL_ADDRESS_OF_YOUR_FRIEND_TEXTBOX).send_keys(email)
+        email_address_of_your_friend = TextField(self.driver,
+                                                 div_id="friend_email")
+        email_address_of_your_friend.input_text(email)
 
     def click_send_to_a_friend_send_button(self):
-        self.driver.find_element(*self.SEND_TO_A_FRIEND_SEND_BUTTON).click()
+        send_to_a_friend_send = Button(self.driver, div_id="sendEmail")
+        send_to_a_friend_send.click()
 
     def click_send_to_a_friend_cancel_button(self):
-        self.driver.find_element(*self.SEND_TO_A_FRIEND_CANCEL_BUTTON).click()
+        send_to_a_friend_cancel = \
+            Button(self.driver,
+                   xpath="//*[@id='send_friend_form_content']/p/a")
+        send_to_a_friend_cancel.click()
 
     def click_send_to_a_friend_x_button(self):
-        self.driver.find_element(*self.SEND_TO_A_FRIEND_X_BUTTON).click()
+        send_to_a_friend_x = Button(self.driver,
+                                    xpath="//*[@title='Close']")
+        send_to_a_friend_x.click()
