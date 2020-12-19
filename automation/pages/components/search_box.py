e9e8bbd217ffb85d30d58df6b049e637c6cfd7bf
mdzirpoli <mdzirpoli@gmail.com>
2020-12-19 16:24:24 -0500

    Refactor search_box with elements.py
    Refactor search_box with elements.py

--- automation/pages/components/search_box.py
+++ automation/pages/components/search_box.py
@@ -1,29 +1,26 @@
 """
 Created on February 8, 2020
+Modified on December 19, 2020
 
 @author: Mark Zirpoli
 
 This module contains methods for the Search Box
 """
 
-from selenium.webdriver.common.by import By
+from automation.elements import Button, TextField
 
 
 class SearchBox(object):
     """
     Page object for Search Box
     """
-
-    # Search box locators
-    SEARCH_US_TEXTBOX = (By.ID, "search_query_top")
-    SEARCH_BUTTON = (By.NAME, "submit_search")
-
     def __init__(self, driver):
         self.driver = driver
 
     def input_search_textbox(self, search):
-        self.driver.find_element(*self.SEARCH_US_TEXTBOX).send_keys(search)
+        search_name = TextField(self.driver, div_id="search_query_top")
+        search_name.input_text(search)
 
     def click_search_button(self):
-        self.driver.find_element(*self.SEARCH_BUTTON).click()
-
+        search_button = Button(self.driver, div_name="submit_search")
+        search_button.click()
